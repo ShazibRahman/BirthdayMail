6d658d4613858bebb8ebb1a1ebb2410ea98425a3
Shazib Rahman <shazibrahman@outlook.com>
2023-04-02 17:19:21 +0530

    commit

--- message.py
+++ message.py
@@ -148,6 +148,7 @@ class BirthdayMail:
             if val not in self.pending_dates:
                 self.pending_dates.append(val)
                 logging.info(f"--adding current date {val['date']} in pending dates list")
+                saveJsontoFile(os.path.join(self.directoryString,'pending_dates.json'))
             if e.__class__.__name__ == "gaierror":
                 logging.info("Network error not able to connect to server.")
                 exit(1)
@@ -167,6 +168,8 @@ class BirthdayMail:
                         self.pending_dates.remove(i)
                         self.dates_done.append(current_date_string)
                         logging.info("--Sending backlog emails succeed for",i['mail'])
+                        saveJsontoFile(os.path.join(self.directoryString,'pending_dates.json'))
+                        saveJsontoFile(os.path.join(self.directoryString,'dates.json'))
                     else:
                         logging.info("--pending dates email failed")
 
