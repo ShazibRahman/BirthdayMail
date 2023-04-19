cf02ba3ffffaa05072e06e1e70e699cb41b7848a
Shazib Rahman <shazibrahman@outlook.com>
2023-04-19 23:35:42 +0530

    commit:

--- utils/csv_to_json.py
+++ utils/csv_to_json.py
@@ -5,7 +5,10 @@ import pathlib
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-
+EMAIL_ADDRESS = "Email Address"
+DATE = "date"
+MAIL = "mail"
+NAME = "name"
 if __name__ == "__main__":
     from gdrive.GDrive import GDrive
     from sort_birthdays_json import main as sort_json
@@ -19,28 +22,28 @@ def csv_json(csv_path: str, json_path: str) -> None:
     )
     names = get_email(data)
     with open(csv_path, encoding="utf-8") as csvFile:
-        csvReader = csv.DictReader(csvFile)
-        for rows in csvReader:
-            if rows["Email Address"] in names:
-                print(rows["Email Address"], "already in the list")
+        csv_reader = csv.DictReader(csvFile)
+        for rows in csv_reader:
+            if rows[EMAIL_ADDRESS] in names:
+                print(rows[EMAIL_ADDRESS], "already in the list")
             else:
-                info = {"mail": rows["Email Address"], "name": rows["name"]}
+                info = {MAIL: rows[EMAIL_ADDRESS], NAME: rows[NAME]}
                 date_data = rows["birth date"].split("/")
                 month, day = date_data[0], date_data[1]
                 if len(month) == 1:
                     month = "0" + month
                 if len(day) == 1:
                     day = "0" + day
-                info["date"] = day + "-" + month
+                info[DATE] = day + "-" + month
                 data.append(info)
-                print(rows["Email Address"], "added to the list")
+                print(rows[EMAIL_ADDRESS], "added to the list")
     sort_json(data)
 
 
 def get_email(data) -> set:
     names = set()
     for i in data:
-        names.add(i["mail"])
+        names.add(i[MAIL])
     return names
 
 
--- utils/sort_birthdays_json.py
+++ utils/sort_birthdays_json.py
@@ -8,7 +8,7 @@ data_path = pathlib.Path(
     __file__).parent.parent.joinpath("data", "data.json")
 
 
-def sortBirthdaysObjectOnKey(birthdays: list, key: str) -> list:
+def sort_birthdays_object_on_key(birthdays: list, key: str) -> list:
     """
     sort birthdays object on key (date)
     """
@@ -19,27 +19,27 @@ def sortBirthdaysObjectOnKey(birthdays: list, key: str) -> list:
     )
 
 
-def readJsonFile(fileName: str) -> list:
-    console.log(f"Reading {fileName}")
-    with open(fileName, "r") as f:
+def read_json_file(file_name: str) -> list:
+    console.log(f"Reading {file_name}")
+    with open(file_name, "r") as f:
         return json.load(f)
 
 
-def writeJsonFile(fileName: str, data: list, indent: int = 4) -> None:
-    console.log(f"Writing {fileName}")
-    with open(fileName, "w") as f:
+def write_json_file(file_name: str, data: list, indent: int = 4) -> None:
+    console.log(f"Writing {file_name}")
+    with open(file_name, "w") as f:
         json.dump(data, f, indent=indent)
 
 
 def main(data) -> None:
     print(data_path)
-    birthdays_sorted = sortBirthdaysObjectOnKey(data, "date")
+    birthdays_sorted = sort_birthdays_object_on_key(data, "date")
     if birthdays_sorted == data:
         console.log("Birthdays are already sorted")
         return
-    writeJsonFile(data_path, birthdays_sorted)
+    write_json_file(data_path, birthdays_sorted)
 
 
 if __name__ == "__main__":
-    data = readJsonFile(data_path)
+    data = read_json_file(data_path)
     main(data)
