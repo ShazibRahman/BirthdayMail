ec4423e4d4296b6ff487c87e4eac3a43d7268ebb
Shazib Rahman <shazibrahman@outlook.com>
2023-09-06 00:40:39 +0530

    commit

--- utils/asymetrcilaEncryDecry.py
+++ utils/asymetrcilaEncryDecry.py
@@ -11,8 +11,27 @@ load_env() # autopep8: off
 
 
 def get_key():
+    """
+    Retrieves the encryption key for the client.
+    Returns:
+        bytes: The encryption key encoded in UTF-8.
+    """
     return os.getenv("client_ency_key").encode("utf-8")
 
 
+
 def decrypt(env):
+    """
+    Decrypts the given encrypted environment variable.
+
+    Args:
+        env (str): The encrypted environment variable to decrypt.
+
+    Returns:
+        str: The decrypted environment variable.
+
+    Raises:
+        ValueError: If the decryption fails.
+    """
     return Fernet(get_key()).decrypt(env.encode("utf-8")).decode("utf-8")
+
